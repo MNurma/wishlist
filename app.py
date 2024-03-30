fdac322a6965f837affc10d23e7a85ba7705bd0e
MNurma <kumauma29official@gmail.com>
2024-03-30 23:47:59 +0700

    First Commit

--- app.py
+++ app.py
@@ -21,39 +21,36 @@ def home():
 
 @app.route("/bucket", methods=["POST"])
 def bucket_post():
-    # sample_receive = request.form['sample_give']
-    bucket_receive = request.form['bucket_give']
-
+    bucket_receive = request.form["bucket_give"]
     count = db.bucket.count_documents({})
     num = count + 1
-
     doc = {
-        'num': num,
+        'num':num,
         'bucket': bucket_receive,
-        'done': 0,
+        'done':0
     }
     db.bucket.insert_one(doc)
-    return jsonify({'msg': 'data saved!'})
+    return jsonify({'msg':'data saved!'})
 
 @app.route("/bucket/done", methods=["POST"])
 def bucket_done():
-    num_receive = request.form['num_give']
+    num_receive = request.form["num_give"]
     db.bucket.update_one(
         {'num': int(num_receive)},
         {'$set': {'done': 1}}
     )
-    return jsonify({'msg': 'update done!'})
+    return jsonify({'msg': 'Update done!'})
 
-@app.route("/delete", methods=["POST"])
+@app.route('/bucket/delete', methods=['POST'])
 def delete_bucket():
-    num_receive = request.form['num_give']
-    db.bucket.delete_one({'num': int(num_receive)})
-    return jsonify({'msg': 'delete done!'})
+     num_receive = int(request.form['num_give'])
+     db.bucket.delete_one({'num': num_receive})
+     return jsonify({'msg' : 'DELETE SUCCESS!!'})
 
 @app.route("/bucket", methods=["GET"])
 def bucket_get():
-    buckets_list = list(db.bucket.find({}, {'_id': False}))
-    return jsonify({'buckets': buckets_list})
+    buckets_list = list(db.bucket.find({},{'_id':False}))
+    return jsonify({'buckets':buckets_list})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
